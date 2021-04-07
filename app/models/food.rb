69cdebd2dd86bc7b776d8eded17b840b25bb27e5
Justin Kufro <kufroj@gmail.com>
2021-04-06 20:47:18 -0600

    I did something

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,9 +2,7 @@
 
 class Food < ApplicationRecord
   validates_numericality_of :amount, greater_than: 0
-
   validates_uniqueness_of :name
-
   validates_presence_of :name
 
   scope :alphabetical, -> { order(name: :asc) }
